cd5626f3b88e4935c9693da7ffde5b3c8a65011c
Maxwell Schorer <maxwell.schorer@coinbase.com>
2021-09-18 14:55:49 -0700

    Fixed to pass tests

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.5.16 <0.9.0;
 
 
 contract SupplyChain {
+  // are runtimes bad?
     address public owner; // internal, private, public?
     uint public skuCount; // internal, private, public?
     mapping (uint => Item) public items; // internal, private, public?\
@@ -54,7 +55,7 @@ contract SupplyChain {
 
     modifier forSale (uint _sku) {
         Item storage item = items[_sku]; // what's purpose of storage keyword?
-        require(item.seller != address(0)); // not sure what to check?
+        require(item.seller != address(0));
         require(item.state == State.ForSale);
         _;
     }
@@ -75,59 +76,52 @@ contract SupplyChain {
     }
 
     constructor() public {
-      // 1. Set the owner to the transaction sender
         owner = msg.sender;
-      // 2. Initialize the sku count to 0. Question, is this necessary?
-        skuCount = 0; // not necessary, default value is 0
+        skuCount = 0; // confirm not necessary
     }
-
-    function addItem(string memory _name, uint _price) public returns (bool) {
-        skuCount += 1;
+    function addItem(string memory _name, uint _price) public returns (bool)
+    {
+        
         items[skuCount] = Item({
-        name: _name, 
-        sku: skuCount, 
-        price: _price, 
-        state: State.ForSale, 
-        seller: msg.sender, 
-        buyer: address(0)
+            name: _name, 
+            sku: skuCount, 
+            price: _price, 
+            state: State.ForSale, 
+            seller: msg.sender, 
+            buyer: address(0)
         });
+        skuCount += 1;
         emit LogForSale(skuCount);
         return true;
     }
 
-  function buyItem(uint sku) payable public // 1. it should be payable in order to receive refunds
-  // 5. this function should use 3 modifiers to check 
-    forSale(sku) //    - if the item is for sale, 
-    paidEnough(sku) //    - if the buyer paid enough, 
-    checkValue(sku) //    - check the value after the function is called to make sure the buyer is refunded any excess ether sent. 
+  function buyItem(uint sku) payable public 
+    forSale(sku)
+    paidEnough(sku)
+    checkValue(sku)
   {
-    items[sku].seller.transfer(msg.value); // 2. this should transfer money to the seller, 
-    items[sku].buyer = msg.sender; // 3. set the buyer as the person who called this transaction, 
-    items[sku].state = State.Sold; // 4. set the state to Sold. 
-    emit LogSold(sku); // 6. call the event associated with this function!
+    items[sku].seller.transfer(items[sku].price);
+    items[sku].buyer = msg.sender;
+    items[sku].state = State.Sold;
+    emit LogSold(sku);
   }
 
   function shipItem(uint sku) public 
-    sold(sku) //    - the item is sold already 
-    // verifyCaller() //    - the person calling this function is the seller. 
+    sold(sku)
+    verifyCaller(items[sku].seller)
   {
-    items[sku].state = State.Shipped; // 2. Change the state of the item to shipped. 
-    emit LogShipped(sku); // 3. call the event associated with this function!
+    items[sku].state = State.Shipped;
+    emit LogShipped(sku);
   }
 
-  // 1. Add modifiers to check 
-  //    - the item is shipped already 
-  //    - the person calling this function is the buyer. 
-  // 2. Change the state of the item to received. 
-  // 3. Call the event associated with this function!
   function receiveItem(uint sku) public 
       shipped(sku)
+      verifyCaller(items[sku].buyer)
   {
     items[sku].state = State.Received;
     emit LogReceived(sku);
   }
 
-  // Uncomment the following code block. it is needed to run tests
   function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) 
   {
    name = items[_sku].name;
